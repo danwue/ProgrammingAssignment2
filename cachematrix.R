0af056be283030b4cd09b7f5bd722bcb8b28f8de
Daniel Würsch <wuersch.daniel@gmail.com>
2016-05-08 17:12:16 +0300

    Implementation of caching inverse of matrices

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+# Allows creation of object wrapper for matrices using makeCacheMatrix which caches computation of inverse when using cacheSolve function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  # Adds caching fields to lexical scope for inverse calculation
+  #
+  # Args:
+  #   x: invertible square matrix
+  #
+  # Returns:
+  #   Object wrapping matrix which caches inverse computation
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) i <<- inverse
+  getinverse <- function() i
+  list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  # Computes inverse matrix if not cached
+  #
+  # Args:
+  #     x: wrapped invertible square matrix obtained through makeCacheMatrix
+  #   ...: additional arguments for solve(...) function
+  #
+  # Returns:
+  #   Inverse of matrix
+  i <- x$getinverse()
+  if (!is.null(i))
+    return(i)
+  data <- x$get()
+  i <- solve(data, ...)
+  x$setinverse(i)
+  i
 }
